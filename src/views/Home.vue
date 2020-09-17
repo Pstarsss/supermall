2673c1dc9a5d4269341fa9a4c2f27993fff231a8
panxing <1377689310@qq.com>
2020-09-17 16:41:35 +0800

    two-commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class=''></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+   return {
+   };
+},
+computed: {
+},
+methods: {
+ },
+created() {
+ },
 }
 </script>
+<style lang='less' scoped>
+</style>
