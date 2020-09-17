5e17d848a2afa04184204efe2aa1e9215ff7ff7c
panxing <1377689310@qq.com>
2020-09-17 16:56:04 +0800

    three-commit

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,7 @@
     
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style>
+@import "./assets/css/base.css";
 
 </style>
